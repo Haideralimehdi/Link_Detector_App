783f870a2fcae45a9fdc8c8e14f47b401e853364
Haideralimehdi <136057229+Haideralimehdi@users.noreply.github.com>
2026-03-18 07:36:09 +0500

    Update constant.dart

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,5 +1,5 @@
 class ApiConstant {
-  static const String _apiKey = "AIzaSyB6AsGzCNuihLhMEQ7g47suenIoZxJWf8E";
+  static const String _apiKey = "your API key";
   static const String endpoint =
       "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=$_apiKey";
 }
